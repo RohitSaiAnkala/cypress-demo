9bd90b26cbd93e4247dd2c56253fa886a97f9c14
rohitsai <rohitsai225@gmail.com>
2021-08-12 00:13:59 +0530

    Cypress basics

---
+++ cypress/integration/selecting.spec.js
@@ -0,0 +1,16 @@
+describe('Text box with max characters', () => {
+	it('displays the appropriate remaining characters count', () => {
+		cy.visit('http://localhost:3000/example-3');
+
+		cy.get('[data-cy="last-name-chars-left-count"]').invoke('text').should('equal', '15');
+		cy.get('[data-cy="input-last-name"]').type('hello');
+		cy.get('[data-cy="last-name-chars-left-count"]').invoke('text').should('equal', '10');
+		cy.get('[data-cy="input-last-name"]').type(' my friend');
+		cy.get('[data-cy="last-name-chars-left-count"]').invoke('text').should('equal', '0');
+	});
+	it('prevents the user from adding more than max chars', () => {
+		cy.visit('http://localhost:3000/example-3');
+		cy.get('[data-cy="input-last-name"]').type('abcdefghijklmnopqrstuvwxyz');
+		cy.get('[data-cy="input-last-name"]').should('have.attr', 'value', 'abcdefghijklmno');
+	});
+});
